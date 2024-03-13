22507faa0eb94fc17ed6c58eae99bb4fe43d33e6
jkwebdeveloperr <jigukoshti3109@gmail.com>
2024-03-13 19:00:30 +0530

    add key

--- src/components/Home/Pampertree.jsx
+++ src/components/Home/Pampertree.jsx
@@ -132,7 +132,7 @@ const Pampertree = () => {
           }}
         >
           {boxData.map((item) => (
-            <SwiperSlide>
+            <SwiperSlide key={item?.id}>
               <div className="w-full relative space-y-4">
                 <div
                   onClick={() => setIsFavourite(!isFavourite)}
