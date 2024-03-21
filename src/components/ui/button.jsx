71b4c3323fb87cb0b5921b07178be9698c35a2ea
TRADteam <nikul.uptechmind@gmail.com>
2024-03-21 13:41:50 +0530

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "px-6 py-5 h-9",
+        default: "px-6 py-3 h-8",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
