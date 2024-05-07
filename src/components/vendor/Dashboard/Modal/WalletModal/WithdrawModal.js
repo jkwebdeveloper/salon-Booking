fab546d2f41fb4af922a75d2d66d85f9f3007fb2
TRADteam <nikul.uptechmind@gmail.com>
2024-05-07 17:54:29 +0530

    update

--- src/components/vendor/Dashboard/Modal/WalletModal/WithdrawModal.js
+++ src/components/vendor/Dashboard/Modal/WalletModal/WithdrawModal.js
@@ -1,17 +1,21 @@
 import Button from "@/components/ui/button";
-import React from "react";
+import React, { useState } from "react";
+import { TbAlertCircle } from "react-icons/tb";
 
 const WithdrawModal = () => {
+  const [selectedOption, setSelectedOption] = useState("paypal");
   return (
     <div className="space-y-4">
       <li class="w-full list-none">
         <div class="flex items-center">
           <input
-            id="list-radio-license"
+            id="paypal-radio"
             type="radio"
-            value=""
-            name="list-radio"
-            class=""
+            value="paypal"
+            name="payment-method"
+            className=""
+            checked={selectedOption === "paypal"}
+            onChange={() => setSelectedOption("paypal")}
           />
           <label
             for="list-radio-license"
@@ -24,11 +28,13 @@ const WithdrawModal = () => {
       <li class="w-full list-none">
         <div class="flex items-center">
           <input
-            id="list-radio-license"
+            id="bank-transfer-radio"
             type="radio"
-            value=""
-            name="list-radio"
-            class=""
+            value="bank-transfer"
+            name="payment-method"
+            className=""
+            checked={selectedOption === "bank-transfer"}
+            onChange={() => setSelectedOption("bank-transfer")}
           />
           <label
             for="list-radio-license"
@@ -39,18 +45,100 @@ const WithdrawModal = () => {
         </div>
       </li>
       <hr />
-      <div className="w-full space-y-1 text-left">
-        <label htmlFor="PayPal Email Address *" className="label_text">
-          PayPal Email Address *
-        </label>
-        <input
-          type="email"
-          name="PayPal Email Address *"
-          className="input_field"
-          placeholder="PayPal Email Address *"
-        />
-      </div>
+      {selectedOption === "paypal" && (
+        <div className="w-full space-y-1 text-left">
+          <label htmlFor="paypal-email" className="label_text">
+            PayPal Email Address *
+          </label>
+          <input
+            type="email"
+            id="paypal-email"
+            name="paypal-email"
+            className="input_field"
+            placeholder="PayPal Email Address *"
+          />
+        </div>
+      )}
+      {selectedOption === "bank-transfer" && (
+        <>
+          <div className="w-full space-y-1 text-left">
+            <label htmlFor="account-text" className="label_text">
+              Name of account holder *
+            </label>
+            <input
+              type="text"
+              id="account-text"
+              name="account-text"
+              className="input_field"
+              placeholder="Type here..."
+            />
+          </div>
+          <div className="w-full space-y-1 text-left">
+            <label htmlFor="account-text" className="label_text">
+              Account number *
+            </label>
+            <input
+              type="text"
+              id="account-text"
+              name="account-text"
+              className="input_field"
+              placeholder="Type here..."
+            />
+          </div>
+          <div className="w-full space-y-1 text-left">
+            <label htmlFor="account-text" className="label_text">
+              Name of the Bank *
+            </label>
+            <input
+              type="text"
+              id="account-text"
+              name="account-text"
+              className="input_field"
+              placeholder="Type here..."
+            />
+          </div>
+        </>
+      )}
       <Button variant="primary">Save</Button>
+
+      {/* Make withdraw request modal start  */}
+
+      {/* <div className="space-y-4">
+        <p className="text-lg font-medium">Withdrawal Amount</p>
+        <p className="text-[#666666]">
+          Available for withdrawal Amount: €1048.00
+        </p>
+        <div className="w-full space-y-1 text-left">
+          <label htmlFor="account-text" className="label_text">
+            Name of account holder *
+          </label>
+          <input
+            type="text"
+            id="account-text"
+            name="account-text"
+            className="input_field"
+            placeholder="Enter your withdrawal amount here..."
+          />
+        </div>
+        <Button variant="primary">Make request</Button>
+      </div> */}
+      {/* Make withdraw request modal End  */}
+
+      {/* withdrawal request Modal start */}
+      {/* <>
+        <div className="flex items-center justify-center">
+          <TbAlertCircle className="text-[64px] text-[#DDB100]" />
+        </div>
+        <p className="text-2xl font-semibold">
+          Are you sure you want to make the <br />
+          withdrawal request?
+        </p>
+        <div className="flex items-center gap-3">
+          <Button variant="primary">Yes, Make it</Button>
+          <Button variant="disable">Cancel</Button>
+        </div>
+      </> */}
+      {/* withdrawal request Modal end */}
     </div>
   );
 };
--- src/components/vendor/Dashboard/Report.js
+++ src/components/vendor/Dashboard/Report.js
@@ -247,6 +247,15 @@ const BusinessReport = () => {
                       <DropdownMenuItem className="flex items-center gap-2 cursor-pointer hover:bg-primary hover:text-white">
                         Last 30 days
                       </DropdownMenuItem>
+                      <DropdownMenuItem className="flex items-center gap-2 cursor-pointer hover:bg-primary hover:text-white">
+                        This month
+                      </DropdownMenuItem>
+                      <DropdownMenuItem className="flex items-center gap-2 cursor-pointer hover:bg-primary hover:text-white">
+                        Last month
+                      </DropdownMenuItem>
+                      <DropdownMenuItem className="flex items-center gap-2 cursor-pointer hover:bg-primary hover:text-white">
+                        Custom range
+                      </DropdownMenuItem>
                     </DropdownMenuGroup>
                   </DropdownMenuContent>
                 </DropdownMenu>
