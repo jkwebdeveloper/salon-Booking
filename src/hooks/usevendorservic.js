e49b0707a38ab11b70ed9de0262e018c61797a71
TRADteam <nikul.uptechmind@gmail.com>
2024-05-16 19:00:34 +0530

    Update usevendorservic.js

--- src/hooks/usevendorservic.js
+++ src/hooks/usevendorservic.js
@@ -1,4 +1,4 @@
-import { POST } from '@/app/api/post';
+import { GET } from '@/app/api/get';
 import React, { useCallback, useEffect } from 'react'
 import { useSelector } from 'react-redux';
 
@@ -7,7 +7,7 @@ const useVendorServices = () => {
     const [vendorServices, setVendorServices] = React.useState({ data: [], loading: true });
 
     const getServices = async () => {
-        const resp = await POST.request({ url: '/vendor/get-vendor-all-services', token: vendor?.api_token });
+        const resp = await GET.request({ url: '/vendor/get-vendor-all-services', token: vendor?.api_token });
         (resp && resp?.code == 200) ? setVendorServices({ data: resp.data, loading: false })
             : setVendorServices({ data: [], loading: false });
     };
