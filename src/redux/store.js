36994adba36b1680ddc46acd45174a70019ab021
Jignesh-koshti <jignesh.theappideas@gmail.com>
2024-05-31 10:32:59 +0530

    set

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,41 @@
-'use client';
+"use client";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { userAuth } from "./features/userAuthSlice";
 import { vendorAuth } from "./features/vendorAuthSlice";
 import {
-    persistReducer, persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from "redux-persist";
-import storage from '@/redux/storage';
+import storage from "@/redux/storage";
 
 const persistConfig = {
-    key: 'root',
-    storage, //Error : storage is imported from 'redux-persist/lib/storage'
-    whitelist: ['userAuth', 'vendorAuth'], // only auth will be persisted
-    version: 1,
+  key: "root",
+  storage, //Error : storage is imported from 'redux-persist/lib/storage'
+  whitelist: ["userAuth", "vendorAuth"], // only auth will be persisted
+  version: 1,
 };
 
 const rootReducer = combineReducers({
-    userAuth: userAuth.reducer,
-    vendorAuth: vendorAuth.reducer,
+  userAuth: userAuth.reducer,
+  vendorAuth: vendorAuth.reducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
