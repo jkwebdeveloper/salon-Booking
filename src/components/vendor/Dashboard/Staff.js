520c3fa3978fe6a4a79ed0516854281f6443e3ec
Jignesh-koshti <jignesh.theappideas@gmail.com>
2024-06-04 18:21:54 +0530

    add key

--- src/components/vendor/Dashboard/Staff.js
+++ src/components/vendor/Dashboard/Staff.js
@@ -100,7 +100,10 @@ const Staff = () => {
                 <tbody>
                   {staffsList.map((staff, index) => {
                     return (
-                      <tr className="bg-white border-b cursor-pointer dark:bg-gray-800 dark:border-gray-700">
+                      <tr
+                        className="bg-white border-b cursor-pointer dark:bg-gray-800 dark:border-gray-700"
+                        key={v4()}
+                      >
                         <td
                           scope="row"
                           className="px-6 py-4 font-medium text-gray-900 capitalize whitespace-nowrap dark:text-white"
