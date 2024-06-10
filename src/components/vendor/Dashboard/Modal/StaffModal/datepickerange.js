58b2d281cd7cd9a01c19592ea846889abbc111ce
Jignesh-koshti <jignesh.theappideas@gmail.com>
2024-06-10 12:34:18 +0530

    week calander

--- src/components/vendor/Dashboard/Modal/StaffModal/datepickerange.js
+++ src/components/vendor/Dashboard/Modal/StaffModal/datepickerange.js
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { CalendarIcon } from "@radix-ui/react-icons";
-import { format, differenceInCalendarDays } from "date-fns";
+import { format, startOfWeek, endOfWeek } from "date-fns";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -26,11 +26,21 @@ export function DatePickerRange({
   });
   const [open, setOpen] = React.useState(defaultOpen);
 
+  const handleSelect = (selectedDate) => {
+    const start = startOfWeek(selectedDate, { weekStartsOn: 0 }); // 0 for Sunday, change to 1 for Monday if needed
+    const end = endOfWeek(selectedDate, { weekStartsOn: 0 });
+    setDate({ from: start, to: end });
+    setOpen(false);
+  };
+
   return (
     <>
       <Popover
         open={open}
-        onOpenChange={(e) => setCalendarOpen && setCalendarOpen(e)}
+        onOpenChange={(e) => {
+          setOpen(e);
+          setCalendarOpen && setCalendarOpen(e);
+        }}
       >
         <PopoverTrigger asChild>
           <Button
@@ -44,33 +54,33 @@ export function DatePickerRange({
           >
             <CalendarIcon className="w-4 h-4 mr-2" />
             {date?.from ? (
-              date.to ?
+              date.to ? (
                 <>
                   {format(date.from, "LLL dd, y")} -{" "}
                   {format(date.to, "LLL dd, y")}
                 </>
-                : format(date.from, "LLL dd, y")
-            ) : <span>Pick a date</span>}
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
           </Button>
         </PopoverTrigger>
         <PopoverContent
           className="w-auto p-0 bg-white"
           align="start"
-          onInteractOutside={(e) => setOpen(!open)}
+          onInteractOutside={() => setOpen(false)}
         >
           <Calendar
             fromDate={new Date()}
             showOutsideDays
             fixedWeeks
-            mode="range"
-            selected={date}
+            mode="single"
+            selected={date.from}
             defaultMonth={date?.from}
             required
-            disabled={new Date()}
-            onSelect={(date) => {
-              setDate(date);
-              setOpen(!open);
-            }}
+            onSelect={handleSelect}
             initialFocus
           />
         </PopoverContent>
