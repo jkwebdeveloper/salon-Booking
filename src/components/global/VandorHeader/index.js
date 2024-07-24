326919ed4437c8d81506c029c4275959d3cc71d8
Jignesh Koshti <jignesh.theappideas@gmail.com>
2024-07-24 15:47:46 +0530

    review modal

--- src/components/global/VandorHeader/index.js
+++ src/components/global/VandorHeader/index.js
@@ -1,85 +1,135 @@
-import Button from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Button from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
 
-import { MdLocationPin } from "react-icons/md";
-import { BiMenuAltRight } from "react-icons/bi";
-import { useSelector } from "react-redux";
+import { MdLocationPin } from 'react-icons/md';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { useSelector } from 'react-redux';
+import { IoIosArrowForward } from 'react-icons/io';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const VandorHeader = () => {
-  const vendor = useSelector((state) => state.vendorAuth.vendor);
-  return (
-    <div className="w-full bg-white shadow-md">
-      <div
-        className="container border-b "
-      >
-        <div className="flex items-center justify-between py-2 md:gap-6">
-          <div className="flex items-center gap-4">
-            <Link href="/vendor/">
-              <Image
-                src={"/static/images/logo.png"}
-                alt="logo"
-                loading="lazy"
-                width={150}
-                height={150}
-              />
-            </Link>
-            <div className="flex items-center gap-6">
-              <Button
-                type="button"
-                variant="outline"
-                size="sm"
-                className="uppercase ps-3 group"
-              >
-                <MdLocationPin className="text-lg transition-all text-primary_color group-hover:text-white" />{" "}
-                Download pampertree app
-              </Button>
-              <div className="block xl:hidden">
-                <BiMenuAltRight className="text-2xl cursor-pointer text-primary_color" />
-              </div>
+    const [openVenderSidebar, setOpenVenderSidebar] = useState(false);
+    const vendor = useSelector(state => state.vendorAuth.vendor);
+    return (
+        <>
+            <div className="hidden w-full bg-white shadow-md lg:block">
+                <div className="container border-b ">
+                    <div className="flex items-center justify-between py-2 md:gap-6">
+                        <div className="flex items-center gap-4">
+                            <Link href="/vendor/">
+                                <Image
+                                    src={'/static/images/logo.png'}
+                                    alt="logo"
+                                    loading="lazy"
+                                    width={150}
+                                    height={150}
+                                />
+                            </Link>
+                            <div className="flex items-center gap-6">
+                                <Button
+                                    type="button"
+                                    variant="outline"
+                                    size="sm"
+                                    className="uppercase ps-3 group"
+                                >
+                                    <MdLocationPin className="text-lg transition-all text-primary_color group-hover:text-white" />{' '}
+                                    Download pampertree app
+                                </Button>
+                                <div className="block xl:hidden">
+                                    <BiMenuAltRight className="text-2xl cursor-pointer text-primary_color" />
+                                </div>
+                            </div>
+                        </div>
+                        {!vendor && (
+                            <p>
+                                Already A Partner?{' '}
+                                <Link href="/vendor/login">
+                                    <span className="uppercase text-primary_color">
+                                        Login
+                                    </span>
+                                </Link>{' '}
+                            </p>
+                        )}
+                        <Button
+                            type="button"
+                            variant="outline"
+                            size="sm"
+                            className="uppercase"
+                            asChild
+                        >
+                            <Link
+                                href={
+                                    (vendor && '/vendor/dashboard') ||
+                                    '/vendor/register'
+                                }
+                            >
+                                {(vendor && 'Dashboard') || 'Become a partner'}
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center ">
+                    <ul className="flex items-center justify-center gap-5 uppercase">
+                        <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
+                            Sell online
+                        </li>
+                        <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
+                            Features
+                        </li>
+                        <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
+                            solutions
+                        </li>
+                        <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
+                            Fees & Pricing
+                        </li>
+                        <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
+                            How it works
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </div>
-          {!vendor && <p>
-            Already A Partner?{" "}
-            <Link href="/vendor/login">
-              <span className="uppercase text-primary_color">Login</span>
-            </Link>{" "}
-          </p>}
-          <Button
-            type="button"
-            variant="outline"
-            size="sm"
-            className="uppercase"
-            asChild
-          >
-            <Link href={vendor && '/vendor/dashboard' || '/vendor/register'}>
-              {vendor && "Dashboard" || "Become a partner"}
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <div className="flex items-center justify-center ">
-        <ul className="flex items-center justify-center gap-5 uppercase">
-          <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
-            Sell online
-          </li>
-          <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
-            Features
-          </li>
-          <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
-            solutions
-          </li>
-          <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
-            Fees & Pricing
-          </li>
-          <li className="px-3 py-3 font-medium cursor-pointer hover:text-white hover:bg-primary">
-            How it works
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+            <div
+                className={`xl:hidden bg-white p-3 space-y-4  text-black absolute top-0 -left-0 z-50 min-h-screen min-w-[100%] ${
+                    openVenderSidebar ? 'translate-x-0' : '-translate-x-full'
+                } transition duration-300 ease-linear`}
+            >
+                <div className="flex items-center justify-between">
+                    <div>
+                        <Image
+                            src={'/static/images/logo.png'}
+                            alt="logo"
+                            loading="lazy"
+                            width={150}
+                            height={150}
+                        />
+                    </div>
+                    <div>
+                        <AiOutlineClose
+                            className="ml-auto text-2xl cursor-pointer"
+                            onClick={() => setOpenVenderSidebar(false)}
+                        />
+                    </div>
+                </div>
+                <div className="flex items-center justify-between">
+                    <p>Profile</p>
+                    <IoIosArrowForward className="text-lg" />
+                </div>
+                <hr className="w-full" />
+                <div className="flex items-center justify-between">
+                    <p>Profile</p>
+                    <IoIosArrowForward className="text-lg" />
+                </div>
+            </div>
+            {openVenderSidebar && (
+                <div
+                    onClick={() => setOpenVenderSidebar(false)}
+                    className="lg:hidden inset-0 z-10 fixed overflow-hidden backdrop-blur-sm bg-lightBlack bg-opacity-50 min-h-screen max-h-screen max-w-[100%]"
+                ></div>
+            )}
+        </>
+    );
 };
 
 export default VandorHeader;
---
+++ src/components/vendor/Dashboard/Modal/ReviewModal.js
@@ -0,0 +1,86 @@
+import Button from '@/components/ui/button';
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const ReviewNewModal = () => {
+    return (
+        <div className="w-full space-y-3">
+            <div className="grid items-start grid-cols-2 gap-10">
+                <div className="space-y-3">
+                    <div className="space-y-2">
+                        <div className="flex items-center gap-2">
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#D6D6D6] text-2xl" />
+                        </div>
+                        <p>Ellen McLaughlin</p>
+                    </div>
+                    <div className="space-y-2">
+                        <p className="text-[#656565]">
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typeset industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s.
+                        </p>
+                        <p>06/06/2024 | Employee: Nuzami</p>
+                    </div>
+                    <textarea
+                        id="message"
+                        rows="4"
+                        className="input_field"
+                        placeholder="Write your thoughts here..."
+                    ></textarea>
+                </div>
+                <div className="p-3 space-y-3">
+                    <div className="space-y-2">
+                        <p className="text-sm">Treatment Rating</p>
+                        <div className="flex items-center gap-2">
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#D6D6D6] text-2xl" />
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="space-y-2">
+                        <p className="text-sm">Ambiance</p>
+                        <div className="flex items-center gap-2">
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#D6D6D6] text-2xl" />
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="space-y-2">
+                        <p className="text-sm">Cleanliness</p>
+                        <div className="flex items-center gap-2">
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#D6D6D6] text-2xl" />
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="space-y-2">
+                        <p className="text-sm">Staff</p>
+                        <div className="flex items-center gap-2">
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#FFCC00] text-2xl" />
+                            <FaStar className="text-[#D6D6D6] text-2xl" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Button variant="primary">Post</Button>
+        </div>
+    );
+};
+
+export default ReviewNewModal;
